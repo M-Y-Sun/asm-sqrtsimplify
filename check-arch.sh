a3f8c3403a4229c14504284978a6209e5f823b28
s7a19t4r <michaelsunsd@gmail.com>
2024-01-03 13:55:07 +0800

    implemented sqrt subroutine changed names of labels of subroutines to make them unique verified correctness of code before the loop

--- check-arch.sh
+++ check-arch.sh
@@ -1,6 +1,6 @@
 #!/bin/bash
 
-if [ $(uname -m) == "arm32" ]; then
+if [ $(uname -m | grep -c "armv") -eq 1 ]; then
     printf "[   \033[1;32mOK\033[0m   ] system architecture matches program architecture\nprogram can be run natively, baremetal\n"
 else
     printf "[ \033[1;31mFAILED\033[0m ] system architecture does not match program architecture\nvirtual machine or emulator is required\n"
