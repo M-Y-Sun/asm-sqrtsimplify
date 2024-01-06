193b6f0a24006d3230a11d36286c3518e928ef91
s7a19t4r <michaelsunsd@gmail.com>
2024-01-06 08:50:56 +0800

    <NOT TESTED> added the forgotten decriment in the compute loop fixed indentation added underscore in front of function names (i.e. 'atoi' -> '_atoi') separated 'itoa.S' into 2 functions, one has the pow macro and the other has _itoa

--- check-arch.sh
+++ check-arch.sh
@@ -1,8 +1,8 @@
 #!/bin/bash
 
 if [ $(uname -m | grep -c "armv") -eq 1 ]; then
-    printf "[   \033[1;32mOK\033[0m   ] system architecture matches program architecture\nprogram can be run natively, baremetal\n"
+    printf "[   \033[1;32mOK\033[0m   ] system architecture matches program architecture\n[  \033[1;37mINFO\033[0m  ] program can be run natively, baremetal\n"
 else
-    printf "[ \033[1;31mFAILED\033[0m ] system architecture does not match program architecture\nvirtual machine or emulator is required\n"
+    printf "[ \033[1;31mFAILED\033[0m ] system architecture does not match program architecture\n[  \033[1;37mINFO\033[0m  ] virtual machine or emulator is required\n"
 fi
 
